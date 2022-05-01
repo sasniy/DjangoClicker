ab60b8e4f544f8f6f6cbb893a89b66b887b01721
Rustam Zhientaev <zhientaev322@gmail.com>
2022-05-01 17:10:54 +0500

    added register,login and logout for user

---
+++ Backend/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import UserForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+
+        return render(request, 'register.html', {'form': form})
+
+    form = UserForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    form = UserForm()
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return redirect('index')
+
+        return render(request, 'login.html', {'form': form, 'invalid': True})
+
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def index(request):
+    return render(request,'index.html')
